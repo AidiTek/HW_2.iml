a1f794fcc4ae1f48adf37f4bea41781703f3d520
AidiTek <12589aitek@gmail.com>
2024-02-12 19:49:28 +0600

    The code has been fixed

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,27 @@
 import java.util.Random;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        permission(23, 34);
-        permission(4, -8);
-        permission(30, 20);
-        permission(20, 9);
-        permission(18, -3);
-        permission(generateRandomAge(), 15);
+        System.out.println(permission(generateRandomAge(), -8));
+        System.out.println(permission(generateRandomAge(), 20));
+        System.out.println(permission(generateRandomAge(), 9));
+        System.out.println(permission(generateRandomAge(), -3));
+        System.out.println(permission(generateRandomAge(), 15));
     }
 
-    public static boolean permission(int age, int temperature) {
-        if (age >= 20 && age < 45 && temperature > -20 && temperature < 30) {
-            System.out.println("Можно идти гулять");
-        } else if (age > 20 && temperature >= 0 && temperature < 28) {
-            System.out.println("Можно идти гулять");
-        } else if (age > 45 && temperature >= 10 && temperature < 25) {
-            System.out.println("Можно идти гулять");
+    public static String permission(int age, int temperature) {
+        if ((age >= 20 && age <= 45 && temperature >= -20 && temperature <= 30) ||
+                (age < 20 && temperature >= 0 && temperature <= 28) ||
+                (age > 45 && temperature >= -10 && temperature <= 25)) {
+            return "Можно идти гулять";
         } else {
-            System.out.println("Остовайтесь дома");
+            return "Оставайтесь дома";
         }
-        return true;
     }
 
     public static int generateRandomAge() {
         Random random = new Random();
-        int rundomnumber = random.nextInt();
+        int rundomnumber = random.nextInt(100);
         return rundomnumber;
     }
 }
